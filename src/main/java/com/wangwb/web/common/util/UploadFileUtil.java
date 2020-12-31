d682cdd3d63c2760c40c74ed216c12d0add26d2f
wangwb-yanmt <729623889@qq.com>
2020-12-31 10:33:15 +0800

    修改附件上传IE8兼容

--- src/main/java/com/wangwb/web/common/util/UploadFileUtil.java
+++ src/main/java/com/wangwb/web/common/util/UploadFileUtil.java
@@ -5,13 +5,18 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import net.sf.json.JSONObject;
 
 /**
  *	文件上传
@@ -100,6 +105,56 @@ public class UploadFileUtil {
 			}
         }
 		return resultMap;
+	}
+	
+	/**
+	 * 	通过transferTo上传  IE8
+	 * @param file 文件流数据
+	 * @param uploadPath 上传路径
+	 * @return
+	 */
+	public static void uploadIE8(MultipartFile file, String uploadPath, HttpServletResponse response) {
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter writer = null;
+		JSONObject object = new JSONObject();
+		try {
+			writer = response.getWriter();
+			if (file==null) {
+				object.put("success", false);
+				object.put("msg", "上传失败，请选择文件");
+			}
+			int limitSize = 3*1024*1024;
+			if(file.getSize() > limitSize) {
+				object.put("success", false);
+				object.put("msg", "上传失败，文件大小需要限制在3M之内");
+			}
+	    	//获取文件名
+			String fileName = file.getOriginalFilename();
+	    	//创建目标文件
+			String uuid = UUIDUtil.getNextValue();
+			String projectPath = uploadPath + uuid + "/";
+	    	File dest = new File(projectPath+fileName);
+	    	//该目标文件如果已存在则删除
+	    	if(dest.exists()) {
+	    		dest.delete();
+	        }
+	        //检测目标文件是否存在
+	    	if (!dest.exists()) {
+	    		dest.mkdirs();
+			}
+	    	file.transferTo(dest);
+	    	object.put("success", true);
+	    	object.put("msg", "上传成功");
+	    	object.put("path", projectPath+fileName);
+	    	object.put("fileName",fileName);
+		} catch (IOException e1) {
+			object.put("success", false);
+			object.put("msg", "上传出现异常");
+		}finally {
+			writer.print(object);
+			writer.flush();
+			writer.close();
+		}
 	}
 	
 }
